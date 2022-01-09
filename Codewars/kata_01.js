382eddaab383740976f92bfa459564b0faf0f648
EmmaFC <emmafecor@hotmail.com>
2022-01-09 18:07:32 +0100

    correting kata code

--- Codewars/kata_01.js
+++ Codewars/kata_01.js
@@ -6,19 +6,23 @@ Given two integers a and b, which can be positive or negative, find the sum of a
 
 export function getSum( a,b ){
     
-    let sum = 0;
-    
     if(a<b){
         for (let i = a; i<=b; i++){
-            sum += i; 
+        sum += i; 
         }
     }
+    
     if(b<a){
         for ( let i = b; i<=a; i++){
-            sum += i; 
+        sum += i; 
         }
     }
-    // console.log(sum);
-    return sum;
+    
+    if(b==a){
+        sum = a;
+    }
+
+   // console.log(sum);
+   return sum;
     
 }
