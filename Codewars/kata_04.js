991d7ded8f297949c84f1179084e52fc5e2a961b
EmmaFC <emmafecor@hotmail.com>
2022-01-13 23:51:44 +0100

    kata_04

---
+++ Codewars/kata_04.js
@@ -0,0 +1,52 @@
+/* Kata_04
+Your task is to write a function called valid_spacing() or validSpacing() which checks if a string has valid spacing. The function should return either True or False.
+
+For this kata, the definition of valid spacing is one space between words, and no leading or trailing spaces. Below are some examples of what the function should return.
+
+'Hello world' = true
+' Hello world' = false
+'Hello world  ' = false
+'Hello  world' = false
+'Hello' = true
+// Even though there are no spaces, it is still valid because none are needed
+'Helloworld' = true 
+// true because we are not checking for the validity of words.
+'Helloworld ' = false
+' ' = false
+'' = true */
+
+export function validSpacing(s) {
+
+  let i =0
+  let a = Array.from(s);
+  let arrayWord=[];
+  let arraySpaces=[];
+  a.forEach((space) => {
+    if(space == ' '){
+      arraySpaces.push(space);
+    }  
+     
+  })
+  let n = 
+ arrayWord = s.split(/[ :;?!~,`"&|()<>{}\[\]\r\n/\\]+/).join('. ').split('.');
+  arrayWord.forEach((x) => {
+console.log(x)   
+     i+1   
+    } )
+console.log(arrayWord) 
+  console.log('expecting: ' + arraySpaces.length+ ' to equal: ' + arrayWord.length)   
+  
+   if(arraySpaces.length == 1 && arrayWord.length == 2){
+    return true
+  }
+ 
+   if(arraySpaces.length == 1 && arrayWord.length == 1){
+    return false
+  }
+   if (arraySpaces.length == arrayWord.length-1){
+    return true
+  }
+  return false
+      
+  
+}
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import {} from './Exercise_07/code.js';
 import {getSum} from './Codewars/kata_01.js';
 import {movie} from './Codewars/kata_02.js';
 import {solution} from './Codewars/kata_03.js';
+import {validSpacing} from './Codewars/kata_04.js';
 
 function app (){
     const ovejas = [
@@ -27,6 +28,7 @@ function app (){
       getSum( 2,50 )
       movie(100, 10, 0.95)
       solution('abc', 'bc')
+      validSpacing('Hello  world')
 }
 
 app ()
